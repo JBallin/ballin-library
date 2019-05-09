f8a39f6d53b8d0c43d3c260d2a1fca662b21ab8f
JBallin <JBallin@users.noreply.github.com>
2019-05-08 20:01:25 -0700

    Enable clickable sorting of fields

--- src/components/SongsTable.jsx
+++ src/components/SongsTable.jsx
@@ -5,37 +5,78 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import Song from './Song';
 
-const SongsTable = ({ songs, tableFields }) => (
-  <Table>
-    <thead>
-      <tr>
-        {
-          tableFields.map(({ title, name, id }) => (
-            <th>
-              {title}
-              {field === name ? <FontAwesomeIcon icon={`caret-${asc ? 'down' : 'up'}`} className="float-right mt-1" /> : null}
-            </th>
-          ))
-        }
-      </tr>
-    </thead>
-    <tbody>
-      {
-        songs.map(song => (
-          <Song song={song} key={song.id} tableFields={tableFields} />
-        ))
-      }
-    </tbody>
-  </Table>
-);
+class SongsTable extends React.Component {
+  handleMouseEnter = (e) => {
+    if (e.target.nodeName === 'TH') {
+      e.target.className = 'bg-light';
+    }
+  }
+
+  handleMouseLeave = (e) => {
+    if (e.target.nodeName === 'TH') {
+      e.target.className = '';
+    }
+  }
+
+  handleMouseDown = (e) => {
+    e.currentTarget.className = 'bg-secondary';
+  }
+
+  handleMouseUp = (e, name) => {
+    const { sortBy } = this.props;
+    e.currentTarget.className = '';
+    sortBy(name);
+  }
+
+  render = () => {
+    const { songs, tableFields, sort: { field, asc } } = this.props;
+    return (
+      <Table>
+        <thead>
+          <tr>
+            {
+              tableFields.map(({ title, name, id }) => (
+                <th
+                  onMouseEnter={this.handleMouseEnter}
+                  onMouseLeave={this.handleMouseLeave}
+                  onMouseDown={this.handleMouseDown}
+                  onMouseUp={e => this.handleMouseUp(e, name)}
+                  style={{ cursor: 'pointer', userSelect: 'none' }}
+                  key={id}
+                >
+                  {title}
+                  {field === name ? <FontAwesomeIcon icon={`caret-${asc ? 'down' : 'up'}`} className="float-right mt-1" /> : null}
+                </th>
+              ))
+            }
+          </tr>
+        </thead>
+        <tbody>
+          {
+            songs.map(song => (
+              <Song song={song} key={song.id} tableFields={tableFields} />
+            ))
+          }
+        </tbody>
+      </Table>
+    );
+  }
+}
 
 SongsTable.propTypes = {
-  songs: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+  songs: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+  })).isRequired,
+  sort: PropTypes.shape({
+    field: PropTypes.string,
+    asc: PropTypes.bool,
+  }).isRequired,
   tableFields: PropTypes.arrayOf(PropTypes.shape({
     title: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     href: PropTypes.string,
   })).isRequired,
+  sortBy: PropTypes.func.isRequired,
 };
 
 export default SongsTable;
